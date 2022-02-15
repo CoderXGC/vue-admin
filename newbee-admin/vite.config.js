9f10d024fc7f175bc478671f2da8e53cf2edb944
CoderXGC <xgc@tom.com>
2022-02-15 10:37:59 +0800

    init

---
+++ newbee-admin/vite.config.js
@@ -0,0 +1,59 @@
+/*import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+import vitePluginImport from 'vite-plugin-babel-import'
+
+// https://vitejs.dev/config/
+export default ({ mode }) => defineConfig({
+  plugins: [
+    vue(),
+    vitePluginImport([
+      {
+        libraryName: 'element-plus',
+        libraryDirectory: 'es'
+      }
+    ])
+  ],
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, './'),
+      '@': path.resolve(__dirname, 'src')
+    },
+  },
+  base: './',
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://backend-api-02.newbee.ltd/manage-api/v1', // 凡是遇到 /api 路径的请求，都映射到 target 属性
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, '') // 重写 api 为 空，就是去掉它
+      }
+    }
+  }
+})*/
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default ({ mode }) => defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, './'),
+      '@': path.resolve(__dirname, 'src')
+    },
+    extensions: ['.vue', '.js', 'jsx', '.json']
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://backend-api-02.newbee.ltd/manage-api/v1', // 凡是遇到 /api 路径的请求，都映射到 target 属性
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, '') // 重写 api 为 空，就是去掉它
+      }
+    }
+  }//,
+ // base: mode == 'development' ? './' : (mode == 'beta' ? '//s.baidu.com/beta/xxx' : '//s.baidu.com/release/xxx') // 静态资源路径配置
+})
