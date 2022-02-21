68b1a6195f82c2b54f1ddefdf36922df11e312ec
CoderXGC <xgc@tom.com>
2022-02-21 09:36:45 +0800

    完成登录

--- newbee-admin/src/App.vue
+++ newbee-admin/src/App.vue
@@ -1,46 +1,85 @@
 <template>
   <div class="layout">
-    <el-container class="container">
+    <el-container v-if="showMenu" class="container">
       <el-aside class="aside">
-        <!--系统名称+logo-->
         <div class="head">
           <div>
             <img src="//s.weituibao.com/1582958061265/mlogo.png" alt="logo">
             <span>vue3 admin</span>
           </div>
         </div>
-        <!--一条为了美观的线条-->
         <div class="line" />
         <el-menu
           background-color="#222832"
           text-color="#fff"
-          router="true"
+          :router="true"
         >
-          <!--一级栏目-->
           <el-submenu index="1">
             <template #title>
               <span>Dashboard</span>
             </template>
-            <!--二级栏目-->
             <el-menu-item-group>
-             <el-menu-item index="/"><i class="el-icon-data-line" />首页</el-menu-item>
+              <el-menu-item index="/"><i class="el-icon-data-line" />首页</el-menu-item>
+              <el-menu-item index="/add"><i class="el-icon-data-line" />添加商品</el-menu-item>
             </el-menu-item-group>
           </el-submenu>
         </el-menu>
       </el-aside>
       <el-container class="content">
-  <div class="main">
-    <!--将 <router-view></router-view> 移到这里，并且用单标签-->
-    <router-view />
-  </div>
-</el-container>
+        <Header />
+        <div class="main">
+          <router-view />
+        </div>
+        <Footer />
+      </el-container>
+    </el-container>
+    <el-container v-else class="container">
+      <router-view />
     </el-container>
   </div>
 </template>
 
 <script>
+import { reactive, toRefs } from 'vue'
+import { useRouter } from 'vue-router'
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
+import { localGet, pathMap } from '@/utils'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Header,
+    Footer
+  },
+  setup() {
+    const noMenu = ['/login']
+    const router = useRouter()
+    const state = reactive({
+      showMenu: true
+    })
+
+    router.beforeEach((to, from, next) => {
+      if (to.path == '/login') {
+        // 如果路径是 /login 则正常执行
+        next()
+      } else {
+        // 如果不是 /login，判断是否有 token
+        if (!localGet('token')) {
+          // 如果没有，则跳至登录页面
+          next({ path: '/login' })
+        } else {
+          // 否则继续执行
+          next()
+        }
+      }
+      state.showMenu = !noMenu.includes(to.path)
+      document.title = pathMap[to.name]
+    })
+
+    return {
+      ...toRefs(state)
+    }
+  }
 }
 </script>
 
@@ -80,48 +119,47 @@ export default {
   border-top: 1px solid hsla(0,0%,100%,.05);
   border-bottom: 1px solid rgba(0,0,0,.2);
 }
+.content {
+  display: flex;
+  flex-direction: column;
+  max-height: 100vh;
+  overflow: hidden;
+}
+.main {
+  height: calc(100vh - 100px);
+  overflow: auto;
+  padding: 10px;
+}
 </style>
 
 <style>
-body {
-  padding: 0;
-  margin: 0;
-  box-sizing: border-box;
-}
-.el-menu {
-  border-right: none!important;
-
-}
-.el-submenu {
-  border-top: 1px solid hsla(0, 0%, 100%, .05);
-  border-bottom: 1px solid rgba(0, 0, 0, .2);
-}
-.el-submenu:first-child {
-  border-top: none;
-}
-.el-submenu [class^="el-icon-"] {
-  vertical-align: -1px!important;
-}
-a {
-  color: #409eff;
-  text-decoration: none;
-}
-.el-pagination {
-  text-align: center;
-  margin-top: 20px;
-}
-.el-popper__arrow {
-  display: none;
-}
-  .content {
-    display: flex;
-    flex-direction: column;
-    max-height: 100vh;
-    overflow: hidden;
+  body {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
   }
-  .main {
-    height: 100vh;
-    overflow: auto;
-    padding: 10px;
+  .el-menu {
+    border-right: none!important;
+  }
+  .el-submenu {
+    border-top: 1px solid hsla(0, 0%, 100%, .05);
+    border-bottom: 1px solid rgba(0, 0, 0, .2);
+  }
+  .el-submenu:first-child {
+    border-top: none;
+  }
+  .el-submenu [class^="el-icon-"] {
+    vertical-align: -1px!important;
+  }
+  a {
+    color: #409eff;
+    text-decoration: none;
+  }
+  .el-pagination {
+    text-align: center;
+    margin-top: 20px;
+  }
+  .el-popper__arrow {
+    display: none;
   }
 </style>
